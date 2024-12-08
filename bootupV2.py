aa14323382f11385b26e1335af0d293b749082e2
Sam Rausch <samrausch@gmail.com>
2024-12-08 14:54:13 -0500

    Still work in progress

--- bootupV2.py
+++ bootupV2.py
@@ -4,7 +4,7 @@ from multiprocessing import Process
 import socket
 import time
 import datetime
-import pytz
+#import pytz
 import RPi.GPIO as GPIO
 import subprocess
 import os
@@ -19,11 +19,15 @@ time.tzset()
 
 run = 1
 
+displayUpdateLast = 0
+displayUpdateDelay = 0.9
+
+
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # button 4
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP) # button 3
+GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP) # button 2
+GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP) # button 1
 
 # Raspberry Pi pin configuration:
 RST = None     # on the PiOLED this pin isnt used
@@ -59,9 +63,9 @@ bottom = height-padding
 x = 4
 
 # Load default font.
-font12 = ImageFont.truetype("/usr/share/fonts/truetype/ttf-bitstream-vera/VeraMono.ttf", 12)
-font14 = ImageFont.truetype("/usr/share/fonts/truetype/ttf-bitstream-vera/VeraMono.ttf", 14)
-font16 = ImageFont.truetype("/usr/share/fonts/truetype/ttf-bitstream-vera/VeraMono.ttf", 16)
+font12 = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf", 12)
+font14 = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf", 14)
+font16 = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf", 16)
 
 
 def get_ip():
@@ -77,82 +81,58 @@ def get_ip():
     finally:
         s.close()
         ip_split = ip.split(".")
-while True:
-       while run == 1: # Idle mode, nothing happening
-           draw.rectangle((0, 0, width, height), outline=0, fill=0)
-           draw.text((x, top),"IP: "+get_ip(), font=font12, fill=255)
-           draw.text((x, bottom-14),time.strftime("%H:%M:%S", time.localtime()), font=font12, fill=255)
-           disp.image(image)
-           disp.display()
 
-           input1 = GPIO.input(4)
-           if input1 == False: # Go to PID mode
-               draw.rectangle((0, 0, width, height), outline=0, fill=0)
-               draw.text((x, top),"Server On Rebooting", font=font12, fill=255)
-               disp.image(image)
-               disp.display()
-               time.sleep(5)
-                bashCommand = "./wirelessServerEnable.sh"
-                process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-                output, error = process.communicate()
-               run = 2
-           input2 = GPIO.input(17)
-           if input2 == False: # Go to Idle mode
-               draw.rectangle((0, 0, width, height), outline=0, fill=0)
-               draw.text((x, top),"Server Off Rebooting", font=font12, fill=255)
-               disp.image(image)
-               disp.display()
-               time.sleep(5)
+ipAddress = get_ip()
+
+def displayUpdate():
+    cmd = "service hostapd status | grep Active | cut -b 14-19"
+    hostapd_status = str(subprocess.check_output(cmd, shell = True ),'utf-8')
+
+    cmd = "service plexmediaserver status | grep Active | cut -b 14-19"
+    plex_status = str(subprocess.check_output(cmd, shell = True ),'utf-8')
+
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+    draw.text((x, top),"IP: "+ipAddress, font=font12, fill=255)
+    draw.text((x, top+12),"HostAPd: "+hostapd_status, font=font12, fill=255)
+    draw.text((x, top+24),"Plex: "+plex_status, font=font12, fill=255)
+    draw.text((x, top+48),time.strftime("%H:%M:%S", time.localtime()), font=font12, fill=255)
+    disp.image(image)
+    disp.display()
+
+    input1 = GPIO.input(4)
+    if input1 == False: # Start wireless server
+        draw.rectangle((0, 0, width, height), outline=0, fill=0)
+        draw.text((x, top),"Turning Server On", font=font12, fill=255)
+        draw.text((x, top+12),"Rebooting...", font=font12, fill=255)
+        disp.image(image)
+        disp.display()
+        time.sleep(5)
+        bashCommand = "./wirelessServerEnable.sh"
+        process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+        output, error = process.communicate()
+    input2 = GPIO.input(17)
+    if input2 == False: # Stop wireless server
+        draw.rectangle((0, 0, width, height), outline=0, fill=0)
+        draw.text((x, top),"Turning Server Off", font=font12, fill=255)
+        draw.text((x, top+12),"Rebooting...", font=font12, fill=255)
+        disp.image(image)
+        disp.display()
+        time.sleep(5)
         bashCommand = "./wirelessServerDisable.sh"
         process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
         output, error = process.communicate()
-               run = 1
-           input3 = GPIO.input(22)
-           if input3 == False:
-               draw.rectangle((0, 0, width, height), outline=0, fill=0)
-               draw.text((x, top),"Shutting Down!", font=font12, fill=255)
-               disp.image(image)
-               disp.display()
-               bashCommand = "shutdown now"
-               process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-               output, error = process.communicate()
-               run = 5
+    input3 = GPIO.input(22)
+    if input3 == False: # Shut down
+        draw.rectangle((0, 0, width, height), outline=0, fill=0)
+        draw.text((x, top),"Shutting Down!", font=font12, fill=255)
+        disp.image(image)
+        disp.display()
+        bashCommand = "shutdown now"
+        process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+        output, error = process.communicate()
+        time.sleep(0.9)
 
-           time.sleep(0.9)
-
-       while run == 2: # PID mode, please be a PID
-           draw.rectangle((0, 0, width, height), outline=0, fill=0)
-           draw.text((x, top),"I'm a PID!", font=font16, fill=255)
-           draw.text((x, bottom-18),time.strftime("%H:%M:%S", time.localtime()), font=font12, fill=255)
-           disp.image(image)
-           disp.display()
-
-           input1 = GPIO.input(4)
-           if input1 == False: # Go to PID mode
-               draw.rectangle((0, 0, width, height), outline=0, fill=0)
-               draw.text((x, top),"Going to PID!", font=font12, fill=255)
-               disp.image(image)
-               disp.display()
-               time.sleep(5)
-               run = 2
-           input2 = GPIO.input(17)
-           if input2 == False: # Go to Idle mode
-               draw.rectangle((0, 0, width, height), outline=0, fill=0)
-               draw.text((x, top),"Going to Idle!", font=font12, fill=255)
-               disp.image(image)
-               disp.display()
-               time.sleep(5)
-               run = 1
-           input3 = GPIO.input(22)
-           if input3 == False:
-               draw.rectangle((0, 0, width, height), outline=0, fill=0)
-               draw.text((x, top),"Shutting Down!", font=font12, fill=255)
-               disp.image(image)
-               disp.display()
-               bashCommand = "shutdown now"
-               process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-               output, error = process.communicate()
-               run = 5
-
-       while run == 5:
-           exit()
+while True:
+    if time.time() > displayUpdateLast + displayUpdateDelay:
+        displayUpdate()
+        displayUpdateLast = time.time()
