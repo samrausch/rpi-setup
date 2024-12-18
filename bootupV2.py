fe8883873d7681d45e63ffb8ad0a198f4437bdeb
Sam Rausch <samrausch@gmail.com>
2024-12-18 12:11:18 -0500

    more gooder

--- bootupV2.py
+++ bootupV2.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3 -u
 
+run = 1
+
 from multiprocessing import Process
 import socket
 import time, threading
 import datetime
-#import pytz
 import RPi.GPIO as GPIO
 import subprocess
 import os
 import Adafruit_SSD1306
 import pyudev
+import psutil
 
 from PIL import Image
 from PIL import ImageDraw
@@ -17,8 +19,6 @@ from PIL import ImageFont
 
 os.environ['TZ'] = "America/New_York"
 time.tzset()
-
-run = 1
 
 displayUpdateLast = 0
 displayUpdateDelay = 0.9
@@ -83,86 +83,115 @@ def get_ip():
         s.close()
         ip_split = ip.split(".")
 
-ipAddress = get_ip()
+#ipAddress = get_ip()
 
-def displayUpdate():
+def displayUpdate(runState):
+  global run
+#  print('{}%'.format(psutil.cpu_percent(interval=2)))
+  if runState == 1:
+    ipAddress = get_ip()
     cmd = "service hostapd status | grep Active"
     hostapd_status = str(subprocess.check_output(cmd, shell = True ),'utf-8').split(":")[1].split(" ")[1]
 
     cmd = "service plexmediaserver status | grep Active"
     plex_status = str(subprocess.check_output(cmd, shell = True ),'utf-8').split(":")[1].split(" ")[1]
 
+    cmd = 'iwgetid |cut -f 2 -d ":"'
+    wifi_status = str(subprocess.check_output(cmd, shell = True ),'utf-8')
+
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
-    draw.text((x, top),"IP: "+ipAddress, font=font12, fill=255)
-    draw.text((x, top+12),"APd: "+hostapd_status, font=font12, fill=255)
-    draw.text((x, top+24),"Plex: "+plex_status, font=font12, fill=255)
-    draw.text((x, top+48),time.strftime("%H:%M:%S", time.localtime()), font=font12, fill=255)
+    draw.text((x, top),ipAddress, font=font12, fill=255)
+    if hostapd_status == "inactive":
+      draw.text((x, top+12),wifi_status, font=font12, fill=255)
+    else:
+      draw.text((x, top+12),hostapd_status, font=font12, fill=255)
+    if plex_status == "active":
+      draw.text((x, top+24),"Plexing", font=font12, fill=255)
+    else:
+      draw.text((x, top+24),"No Plex", font=font12, fill=255)
+    draw.text((x, top+48),'{}%'.format(psutil.cpu_percent()), font=font12, fill=255)
+    draw.text((x+64, top+48),time.strftime("%H:%M:%S", time.localtime()), font=font12, fill=255)
+    disp.image(image)
+    disp.display()
+  elif runState == 2:
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+    draw.text((x, top),"Stuff goes here", font=font12, fill=255)
+    draw.text((x+64, top+48),time.strftime("%H:%M:%S", time.localtime()), font=font12, fill=255)
+    disp.image(image)
+    disp.display()
+  else:
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+    draw.text((x, top),"I'm broken...", font=font12, fill=255)
+    draw.text((x+64, top+48),time.strftime("%H:%M:%S", time.localtime()), font=font12, fill=255)
     disp.image(image)
     disp.display()
 
-    input1 = GPIO.input(4)
-    if input1 == False: # Start wireless server
-        draw.rectangle((0, 0, width, height), outline=0, fill=0)
-        draw.text((x, top),"Turning Server On", font=font12, fill=255)
-        draw.text((x, top+12),"Rebooting...", font=font12, fill=255)
-        disp.image(image)
-        disp.display()
-        time.sleep(5)
-        bashCommand = "./wirelessServerEnable.sh"
-        process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-        output, error = process.communicate()
-    input2 = GPIO.input(17)
-    if input2 == False: # Stop wireless server
-        draw.rectangle((0, 0, width, height), outline=0, fill=0)
-        draw.text((x, top),"Turning Server Off", font=font12, fill=255)
-        draw.text((x, top+12),"Rebooting...", font=font12, fill=255)
-        disp.image(image)
-        disp.display()
-        time.sleep(5)
-        bashCommand = "./wirelessServerDisable.sh"
-        process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-        output, error = process.communicate()
-    input3 = GPIO.input(22)
-    if input3 == False: # Shut down
-        draw.rectangle((0, 0, width, height), outline=0, fill=0)
-        draw.text((x, top),"Shutting Down!", font=font12, fill=255)
-        disp.image(image)
-        disp.display()
-        bashCommand = "shutdown now"
-        process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-        output, error = process.communicate()
-        time.sleep(0.9)
-    threading.Timer(0.9, displayUpdate).start()
+
+
+  input1 = GPIO.input(4)
+  if input1 == False: # Start wireless server
+      draw.rectangle((0, 0, width, height), outline=0, fill=0)
+      draw.text((x, top),"Turning Server On", font=font12, fill=255)
+      draw.text((x, top+12),"Rebooting...", font=font12, fill=255)
+      disp.image(image)
+      disp.display()
+      time.sleep(5)
+      bashCommand = "./wirelessServerEnable.sh"
+      process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+      output, error = process.communicate()
+  input2 = GPIO.input(17)
+  if input2 == False: # Stop wireless server
+      draw.rectangle((0, 0, width, height), outline=0, fill=0)
+      draw.text((x, top),"Turning Server Off", font=font12, fill=255)
+      draw.text((x, top+12),"Rebooting...", font=font12, fill=255)
+      disp.image(image)
+      disp.display()
+      time.sleep(5)
+      bashCommand = "./wirelessServerDisable.sh"
+      process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+      output, error = process.communicate()
+  input3 = GPIO.input(22)
+  if input3 == False: # Shut down
+      draw.rectangle((0, 0, width, height), outline=0, fill=0)
+      draw.text((x, top),"Shutting Down!", font=font12, fill=255)
+      disp.image(image)
+      disp.display()
+      bashCommand = "shutdown now"
+      process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+      output, error = process.communicate()
+      time.sleep(0.9)
+  input4 = GPIO.input(27)
+  if input4 == False: # Switch run
+    if run == 1:
+      run = 2
+    else:
+      run = 1
+
+  threading.Timer(0.9, displayUpdate, args=(run,)).start()
 
 def monitorBlockDevices():
-    context = pyudev.Context()
-    monitor = pyudev.Monitor.from_netlink(context)
-    monitor.filter_by(subsystem='block')
+  context = pyudev.Context()
+  monitor = pyudev.Monitor.from_netlink(context)
+  monitor.filter_by(subsystem='block')
 
-    for device in iter(monitor.poll, None):
-        if device.action == 'add':
-            print(device.device_node)
-            if device.device_node[-1].isdigit():
-                cmd = "mount "+device.device_node+" /mnt/usb"+device.device_node[-4:]
-                mount_status = str(subprocess.check_output(cmd, shell = True ),'utf-8')
-                print(mount_status)
-                break
-        if device.action == 'remove':
-            print(device.device_node)
-            if device.device_node[-1].isdigit():
-                cmd = "umount "+device.device_node
-                umount_status = str(subprocess.check_output(cmd, shell = True ),'utf-8')
-                print(umount_status)
-                break
-    threading.Timer(0.01, monitorBlockDevices).start()
+  for device in iter(monitor.poll, None):
+      if device.action == 'add':
+          print(device.device_node)
+          if device.device_node[-1].isdigit():
+              cmd = "mount "+device.device_node+" /mnt/usb"+device.device_node[-4:]
+              mount_status = str(subprocess.check_output(cmd, shell = True ),'utf-8')
+              print(mount_status)
+              break
+      if device.action == 'remove':
+          print(device.device_node)
+          if device.device_node[-1].isdigit():
+              cmd = "umount "+device.device_node
+              umount_status = str(subprocess.check_output(cmd, shell = True ),'utf-8')
+              print(umount_status)
+              break
+  threading.Timer(0.01, monitorBlockDevices).start()
 
-
-
-#while True:
-#    if time.time() > displayUpdateLast + displayUpdateDelay:
-#        displayUpdate()
-#        displayUpdateLast = time.time()
-displayUpdate()
+displayUpdate(run)
 monitorBlockDevices()
 while True:
-    time.sleep(0.001)
+  time.sleep(0.001)
